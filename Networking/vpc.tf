a1b2b3f511911d853e063198543bb9cb1be8d7b6
skvladimir <46741493+skvladimir@users.noreply.github.com>
2024-06-04 12:59:04 +0300

    add rg

--- Networking/vpc.tf
+++ Networking/vpc.tf
@@ -1,7 +1,4 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "Poland Central"
-}
+
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"

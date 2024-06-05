0f6178c70de8c18f0195176f4ec97941ea8ded72
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-05 12:54:49 +0200

    add vm

---
+++ project/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.106.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  # Configuration options
+}
+
+provider "tls" {}

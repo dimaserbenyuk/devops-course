9566945a2e5100faec2224a9e473a0e513c588f7
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-06 16:25:00 +0200

    fix

--- project/vpc.tf
+++ project/vpc.tf
@@ -4,29 +4,29 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Define the virtual network
-resource "azurerm_virtual_network" "vpc" {
-  name                = "my-network"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+# resource "azurerm_virtual_network" "vpc" {
+#   name                = "my-network"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   address_space       = ["10.0.0.0/16"]
+#   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-    # security_group = azurerm_network_security_group.private.id
-  }
+#   subnet {
+#     name           = "subnet1"
+#     address_prefix = "10.0.1.0/24"
+#     # security_group = azurerm_network_security_group.private.id
+#   }
 
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-    # security_group = azurerm_network_security_group.private.id
-  }
+#   subnet {
+#     name           = "subnet2"
+#     address_prefix = "10.0.2.0/24"
+#     # security_group = azurerm_network_security_group.private.id
+#   }
 
-  tags = {
-    environment = "Production"
-  }
-}
+#   tags = {
+#     environment = "Production"
+#   }
+# }
 
 # # Define the private network security group with rules for SSH, HTTP, and HTTPS
 # resource "azurerm_network_security_group" "private" {

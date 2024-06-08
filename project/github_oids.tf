70c2f0c667e651cd024168ece8cb2a33308e52f4
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-08 10:06:04 +0200

    fix

--- project/github_oids.tf
+++ project/github_oids.tf
@@ -88,10 +88,19 @@ resource "azuread_application" "this" {
   }
 }
 
+
+# resource "azuread_service_principal" "service_principal" {
+#   application_id = azuread_application.app_service_principal.application_id
+#   app_role_assignment_required = false
+#   owners = [
+#     data.azuread_client_config.current.object_id
+#   ]
+# }
+
 resource "azuread_service_principal" "this" {
   client_id                    = azuread_application.this.client_id
   app_role_assignment_required = false
-  #   owners                       = [data.azuread_client_config.current.object_id, data.azuread_user.this.object_id]
+  owners                       = [data.azuread_client_config.current.object_id, data.azuread_user.this.object_id]
   #   lifecycle {
   #     prevent_destroy = false
   #   }

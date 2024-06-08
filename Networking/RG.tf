8c1f7fe408d6da8c0f47da3551730bf1f6f972d5
DmytroK <lol.to95@gmail.com>
2024-06-08 12:42:11 +0300

    dell RG

--- Networking/RG.tf
+++ Networking/RG.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "DK-m" {
   name     = "DK-resources"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "DK-t" {
-  name     = "DK-resources"
-  location = "West Europe"
-}
--- Networking/location.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "WP" {
-  name     = "DKmytro"
-  location = "East US"
-}

4a2b3af66f4795739f4c9f496a0d7041754ae407
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-08 10:35:42 +0200

    output

--- project/outputs.tf
+++ project/outputs.tf
@@ -8,27 +8,27 @@
 
 # }
 
-output "sp_client_id" {
-  value       = azuread_service_principal.this.client_id
-  description = "The service principal application id"
-}
+# output "sp_client_id" {
+#   value       = azuread_service_principal.this.client_id
+#   description = "The service principal application id"
+# }
 
-output "sp_password" {
-  value       = azuread_application_password.this.value
-  description = "The service principal password"
-  sensitive   = true
-}
+# output "sp_password" {
+#   value       = azuread_application_password.this.value
+#   description = "The service principal password"
+#   sensitive   = true
+# }
 
 output "sp_application_name" {
   value       = azuread_application.this.display_name
   description = "The name of the application"
 }
 
-output "sp_object_id" {
-  value       = azuread_service_principal.this.object_id
-  description = "Object ID for the service principal"
-}
+# output "sp_object_id" {
+#   value       = azuread_service_principal.this.object_id
+#   description = "Object ID for the service principal"
+# }
 
-output "credential_id" {
-  value = azuread_application_federated_identity_credential.main.id
-}
+# output "credential_id" {
+#   value = azuread_application_federated_identity_credential.main.id
+# }

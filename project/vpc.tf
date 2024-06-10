e8da43cf6aa15276ac9e4a82ebe34e1d83cd7599
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-10 13:41:00 +0200

    fix

--- project/vpc.tf
+++ project/vpc.tf
@@ -16,13 +16,13 @@ resource "azurerm_virtual_network" "vpc" {
   subnet {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
-    security_group = azurerm_network_security_group.private.id
+    # security_group = azurerm_network_security_group.private.id
   }
 
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.private.id
+    # security_group = azurerm_network_security_group.private.id
   }
 
   tags = {

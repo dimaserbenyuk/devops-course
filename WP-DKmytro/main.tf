1d287430e3ef75ba068fc917de68a96631f9228f
DmytroK <lol.to95@gmail.com>
2024-06-11 23:19:06 +0300

    fix sql1.0

--- WP-DKmytro/main.tf
+++ WP-DKmytro/main.tf
@@ -119,13 +119,13 @@ resource "azurerm_virtual_machine" "example" {
   }
 }
 
-resource "azurerm_mysql_server" "example" {
+resource "azurerm_mysql_flexible_server" "example" {
   name                = "wordpress-mysql-server"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
+  administrator_password = "H@Sh1CoR3!"
 
   sku_name   = "B_Gen5_1"
   storage_mb = 5120

77a192b55b172a634a5054654b6baa06712d44d6
DmytroK <lol.to95@gmail.com>
2024-06-13 17:15:36 +0300

    please fix sql1.1

--- WP-DKmytro/main.tf
+++ WP-DKmytro/main.tf
@@ -19,7 +19,48 @@ resource "azurerm_subnet" "example" {
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
+  #   service_endpoints    = ["Microsoft.Storage"]
+
+  #   delegation {
+  #     name = "fs"
+  #     service_delegation {
+  #       name = "Microsoft.DBforMySQL/flexibleServers"
+  #       actions = [
+  #         "Microsoft.Network/virtualNetworks/subnets/join/action",
+  #       ]
+  #     }
+  # }
 }
+
+resource "azurerm_subnet" "example-teatb2" {
+  name                 = "wordpress-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "example" {
+  name                = "example.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "exampleVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.example.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+  resource_group_name   = azurerm_resource_group.example.name
+}
+
 
 resource "azurerm_public_ip" "example" {
   name                = "wordpress-public-ip"
@@ -119,34 +160,49 @@ resource "azurerm_virtual_machine" "example" {
   }
 }
 
+# resource "azurerm_mysql_flexible_server" "example" {
+#   name                = "wordpress-mysql-server"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+
+#   administrator_login          = "mysqladminun"
+#   administrator_password = "H@Sh1CoR3!"
+
+#   sku_name   = "B_Gen5_1"
+
+#   version    = "5.7"
+
+#   auto_grow_enabled             = true
+#   backup_retention_days         = 7
+#   geo_redundant_backup_enabled  = false
+#   infrastructure_encryption_enabled = false
+#   public_network_access_enabled = true
+#   ssl_enforcement_enabled       = true
+# }
+
+# resource "azurerm_mysql_database" "example" {
+#   name                = "wordpressdb"
+#   resource_group_name = azurerm_resource_group.example.name
+#   server_name         = azurerm_mysql_server.example.name
+#   charset             = "utf8"
+#   collation           = "utf8_general_ci"
+# }
+
+# output "public_ip_address" {
+#   value = azurerm_public_ip.example.ip_address
+# }
+
+
 resource "azurerm_mysql_flexible_server" "example" {
-  name                = "wordpress-mysql-server"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  administrator_login          = "mysqladminun"
+  name                   = "exampletesta1"
+  resource_group_name    = azurerm_resource_group.example.name
+  location               = azurerm_resource_group.example.location
+  administrator_login    = "psqladmin"
   administrator_password = "H@Sh1CoR3!"
+  backup_retention_days  = 7
+  delegated_subnet_id    = azurerm_subnet.example.id
+  private_dns_zone_id    = azurerm_private_dns_zone.example.id
+  sku_name               = "GP_Standard_D2ds_v4"
 
-  sku_name   = "B_Gen5_1"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled             = true
-  backup_retention_days         = 7
-  geo_redundant_backup_enabled  = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled = true
-  ssl_enforcement_enabled       = true
-}
-
-resource "azurerm_mysql_database" "example" {
-  name                = "wordpressdb"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_mysql_server.example.name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
-}
-
-output "public_ip_address" {
-  value = azurerm_public_ip.example.ip_address
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
 }

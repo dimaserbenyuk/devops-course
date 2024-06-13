15e537a42971cd7192748fa66351fa4de5b3e028
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-13 20:40:18 +0200

    add

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "resource_group_location" {
   type    = string
   default = "West Europe"
 }
+
+# variable "name_prefix" {
+#   default     = "postgresqlfs"
+#   description = "Prefix of the resource name."
+# }
+
+# variable "location" {
+#   default     = "eastus"
+#   description = "Location of the resource."
+# }

38f1a04c26ccb82739c1ca46624a67afbaf66638
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-06-18 12:29:40 +0200

    fix

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,12 +1,3 @@
-// import { authMiddleware } from "@clerk/nextjs";
-
-// export default authMiddleware({});
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware();

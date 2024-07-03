ff8d9cbf4109014a86eb774a12dc1cb07f90d60f
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-07-03 20:42:16 +0200

    add

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -4,63 +4,63 @@ resource "azurerm_resource_group" "rg-aks" {
 }
 
 
-# resource "azurerm_kubernetes_cluster" "aks" {
-#   name                = "aks"
-#   kubernetes_version  = "1.29"
-#   location            = azurerm_resource_group.rg-aks.location
-#   resource_group_name = azurerm_resource_group.rg-aks.name
-#   dns_prefix          = "exampleaks"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks"
+  kubernetes_version  = "1.29"
+  location            = azurerm_resource_group.rg-aks.location
+  resource_group_name = azurerm_resource_group.rg-aks.name
+  dns_prefix          = "exampleaks"
 
 
-#   default_node_pool {
-#     name       = "default"
-#     node_count = 2
-#     vm_size    = "Standard_D2_v2"
-#   }
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-#   network_profile {
-#     load_balancer_sku = "standard"
-#     network_plugin    = "kubenet"
-#   }
-# }
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet"
+  }
+}
 
-# output "client_certificate" {
-#   value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
-#   sensitive = true
-# }
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive = true
+}
 
-# output "kube_config" {
-#   value = azurerm_kubernetes_cluster.aks.kube_config_raw
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
 
-#   sensitive = true
-# }
+  sensitive = true
+}
 
-# resource "helm_release" "external_nginx" {
-#   name = "external"
+resource "helm_release" "external_nginx" {
+  name = "external"
 
-#   repository       = "https://kubernetes.github.io/ingress-nginx"
-#   chart            = "ingress-nginx"
-#   namespace        = "ingress"
-#   create_namespace = true
-#   version          = "4.8.0"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress"
+  create_namespace = true
+  version          = "4.8.0"
 
-#   values = [file("${path.module}/values/ingress.yaml")]
-# }
+  values = [file("${path.module}/values/ingress.yaml")]
+}
 
-# resource "helm_release" "cert_manager" {
-#   name = "cert-manager"
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
 
-#   version          = "v1.15.0"
-#   chart            = "jetstack/cert-manager"
-#   namespace        = "cert-manager"
-#   create_namespace = true
+  version          = "v1.15.0"
+  chart            = "jetstack/cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
 
-#   set {
-#     name  = "installCRDs"
-#     value = "true"
-#   }
-# }
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}

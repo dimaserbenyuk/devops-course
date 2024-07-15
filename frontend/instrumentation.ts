b4312cc43c42c22585829db73403b47e0921dc07
Dmytro Serbeniuk <dmytro.serbeniuk@lotusflare.com>
2024-07-15 13:31:00 +0200

    fix: update front

--- frontend/instrumentation.ts
+++ frontend/instrumentation.ts
@@ -3,6 +3,7 @@ export async function register() {
     await import('./sentry.server.config');
   }
 
+  
   if (process.env.NEXT_RUNTIME === 'edge') {
     await import('./sentry.edge.config');
   }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
+
 // Injected content via Sentry wizard below
 
 const { withSentryConfig } = require("@sentry/nextjs");
